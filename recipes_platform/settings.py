57358c3dca96e1a293249c68e76079065f033156
Rohit Varathe <117350053+rohitrkvarathe111@users.noreply.github.com>
2025-08-22 21:53:18 +0530

    Update settings.py

--- recipes_platform/settings.py
+++ recipes_platform/settings.py
@@ -187,8 +187,8 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "rohitvarathe3@gmail.com"  
-EMAIL_HOST_PASSWORD = "ucsshzqkercvyarn"     
+EMAIL_HOST_USER = "rohitvarathe@gmail.com"  
+EMAIL_HOST_PASSWORD = "ucqkercvyarn"     
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
